be5b4f1fba968323abd7811d152364637b812a16
Juliana <jmafra.pe@gmail.com>
2025-09-15 20:44:08 -0300

    Adicionando testes com mocks na controller

--- test/controller/checkoutController.test.js
+++ test/controller/checkoutController.test.js
@@ -62,6 +62,53 @@ it('Quando o checkout é realizado com sucesso, retorna 200', async () => {
         expect(resposta.body).to.have.property('total');       
        
     });
+
+     it('Usando Mocks: Quando os dados do checkout são inválidos, retorna 400', async () => {
+       const checkoutMock = sinon.stub(checkoutService, 'checkout').throws(new Error('Dados do checkout inválidos'));   
+       checkoutMock.throws(new Error('Dados do checkout inválidos'));
+       
+        const resposta = await request(app)
+            .post('/api/checkout')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                // Dados incompletos ou inválidos-sem items
+                freight: 10,
+                paymentMethod: 'credit_card',
+                cardData: { number: '1234-5678-9012-3456', expiry: '12/25', cvv: '123' },
+            });
+
+        expect(resposta.status).to.equal(400);
+        expect(resposta.body).to.have.property('error');
+        checkoutMock.restore();
+    });
+
+    it('Usando Mocks: Quando o checkout é realizado com sucesso, retorna 200', async () => {
+        const checkoutMock = sinon.stub(checkoutService, 'checkout').returns({
+            userId: 1,
+            items: [{ productId: 1, quantity: 2 }],     
+            freight: 10,
+            paymentMethod: 'credit_card',
+            total: 100.00,
+        });
+        
+        const resposta = await request(app)
+            .post('/api/checkout')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                items: [{ productId: 1, quantity: 2 }],
+                freight: 10,
+                paymentMethod: 'credit_card',
+                cardData: { number: '1234-5678-9012-3456', expiry: '12/25', cvv: '123' },
+            });
+
+        expect(resposta.status).to.equal(200);  
+        expect(resposta.body).to.have.property('valorFinal');       
+        expect(resposta.body).to.have.property('items');
+        expect(resposta.body).to.have.property('freight');
+        expect(resposta.body).to.have.property('paymentMethod');
+        expect(resposta.body).to.have.property('total');       
+        checkoutMock.restore();
+    });
     
     });
   });
